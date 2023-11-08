9e33a3aa46f74384bc1ddc2dfda6d46a4c582d62
Refdinal <136530969+Refdinal@users.noreply.github.com>
2023-11-08 17:56:38 +0700

    Update hals.py

--- hals.py
+++ hals.py
@@ -2,14 +2,13 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import folium
+import plotly_express as px
 import xlsxwriter
 from streamlit_folium import st_folium
 import io
 
-st.markdown(
-    "<h1 style='text-align: center; color: White;'>Data Survey HALS</h1>",
-    unsafe_allow_html=True,
-)
+
+st.header("Data Survey HALS")
 
 
 @st.cache_resource()
@@ -78,27 +77,13 @@ with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
     df_download.to_excel(writer, sheet_name="Sheet1", index=False)
 
 download2 = st.download_button(
-    label="Download Excel",
+    label="Download Data Survey HALS.xlsx",
     data=buffer,
     file_name="Data_survey_HALS.xlsx",
     mime="application/vnd.ms-excel",
 )
 
-# @st.cache_resource()
-# def convert_to_csv(df):
-#     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-#     return df.to_csv(index=False).encode("utf-8")
 
-
-# csv = convert_to_csv(df)
-
-# # display the dataframe on streamlit app
-
-
-# # download button 1 to download dataframe as csv
-# download1 = st.download_button(
-#     label="Download data as CSV", data=csv, file_name="large_df.csv", mime="text/csv"
-# )
 df["Google Map Link"] = (
     "http://www.google.com/maps/place/"
     + df["Latitude"].astype(str)
@@ -107,14 +92,31 @@ df["Google Map Link"] = (
 )
 location = df[["Nama", "Latitude", "Longitude", "Google Map Link"]]
 location_list = location.to_dict("records")
-locationx = location_list[0]
-map = folium.Map(location=[-0.3077269515839052, 100.01030796451599], zoom_start=12)
+
+map = folium.Map(location=[-0.3077269515839052, 100.01030796451599], zoom_start=14)
 for locationx in location_list:
     coords = (locationx["Latitude"], locationx["Longitude"])
     link = locationx["Google Map Link"]
     pop = "<a href=" + link + " target=”_blank”>" + locationx["Nama"] + "</a>"
     folium.Marker(coords, popup=pop).add_to(map)
-
 st.title("Titik Lokasi Penerima Manfaat")
+st.text("Klik pada marker dan klik Nama Pemanfaat untuk menuju ke Google Map")
 st_data = st_folium(map, width=700, height=400)
-# download button 2 to download dataframe as xlsx
+st.title("Rincian Data")
+col1, col2 = st.columns(2)
+pemanfaat = len(df)
+pemanfaat = str(pemanfaat) + " Rumah"
+col1.metric("Jumlah Daftar Pemanfaat", pemanfaat)
+jiwa = df["Jumlah Jiwa"].sum()
+jiwa = str(jiwa) + " Orang"
+col2.metric("Jumlah Jiwa", jiwa)
+
+
+st.subheader("Jumlah Pemanfaat Berdasarkan Jorong")
+perjorong = df.groupby(by=["Jorong"]).count()[["Nama"]].sort_values(by="Nama")
+perjorong.rename(columns={"Nama": "Jumlah Pemanfaat"}, inplace=True)
+
+fig = px.bar(
+    perjorong, x="Jumlah Pemanfaat", y=perjorong.index, orientation="h", text_auto=True
+)
+st.plotly_chart(fig)
